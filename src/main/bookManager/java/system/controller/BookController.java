eae4bb94e7a388250de11b76b59961a3ba653679
JeckerWen <JeckerWen@gmail.com>
2018-11-02 16:02:24 +0800

    bookService

---
+++ src/main/bookManager/java/system/controller/BookController.java
@@ -0,0 +1,11 @@
+package bookManager.java.system.controller;
+
+import bookManager.java.common.support.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("book")
+public class BookController extends BaseController {
+
+}
